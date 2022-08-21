3f930d566e2629b72167fe5deac3a3c7698f40d3
sylwiastolz <sylwiastolz@interia.pl>
2022-08-21 17:04:02 +0200

    paris

--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div classNameName="wholepage">
-      <h1>
-        <ul>
-          <li>New York</li>
-          <li>Monday, 05.09.2023</li>
-          <li>Rainy</li>
-        </ul>
-      </h1>
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
 
-      <div className="row">
-        <div className="col-5">
-          <ul>
-            <li>Humidity:60%</li>
-            <li>Wind:21 km/h</li>
-          </ul>
-        </div>
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
 
-        <div className="col-2">
-          <img className="picture" src="#" alt="weatherpicture" />
-        </div>
-        <div className="col-5">
-          <span className="temperature">
-            <span className="temp">31 C</span>
-          </span>
-        </div>
-      </div>
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=58b3466d5113849cc0dbfb1ee306eb84&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
 
-      <form className="console">
-        <input
-          type="search"
-          placeholder="Enter name of the city"
-          autoComplete="of"
-        />
-        <input className="button" type="submit" value="Search" />
-        <br />
-        <input className="button" type="submit" value="Weather in your city" />
-      </form>
-    </div>
+  function newCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form className="console" onSubmit={handleSubmit}>
+      <input
+        onChange={newCity}
+        type="search"
+        placeholder="Enter name of the city"
+        autoComplete="of"
+      />
+      <br />
+      <br />
+      <button>Search</button>
+      <br />
+      <br />
+      <button>Weather in your city</button>
+    </form>
   );
+
+  if (loaded) {
+    return (
+      <div>
+        <h1>
+          <ul>
+            <li>{weather.city}</li>
+            <li>Friday 20.08.2022</li>
+            <li>{weather.description} </li>
+          </ul>
+        </h1>
+        <br />
+        <div className="row">
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {Math.round(weather.humidity)} %</li>
+              <li>Wind: {Math.round(weather.wind)} km/h</li>
+            </ul>
+          </div>
+
+          <div className="col-1">
+            <img src={weather.icon} alt="weathericonsearch" />
+          </div>
+
+          <div className="col-1"></div>
+          <div className="col-3">
+            <span className="temperature">
+              {Math.round(weather.temperature)}°C
+            </span>
+          </div>
+        </div>
+        {form}
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
